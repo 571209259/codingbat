3c0ca45a223a779b8836809e4b3c8b6ea86c8a31
571209259 <54323388+571209259@users.noreply.github.com>
2019-09-18 07:54:26 -0500

    Update nonStart.java

--- java/string-1/nonStart.java
+++ java/string-1/nonStart.java
@@ -1,6 +1,8 @@
 /* Given 2 strings, return their concatenation, except omit the first char of 
  * each. The strings will be at least length 1.
  */
+String a = "Tyler"
+String b = "Dwinal"
 public String nonStart(String a, String b) {
     return a.substring(1) + b.substring(1);
 }
